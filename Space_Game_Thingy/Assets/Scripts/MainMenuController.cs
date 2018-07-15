55e8ab8314320615dfddb62085d7dfcb7cf81ad0
Suius <31655841+Suius@users.noreply.github.com>
2018-07-15 17:34:36 +0300

    Fixed missing script
    Yeah.

---
+++ Space_Game_Thingy/Assets/Scripts/MainMenuController.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class MainMenuController : MonoBehaviour
+{
+
+    public Text NameOfTheGame;
+
+    void Start()
+    {
+        NameOfTheGame.text = "Star Invasion X";
+    }
+}
